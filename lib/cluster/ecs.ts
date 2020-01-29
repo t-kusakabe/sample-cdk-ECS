d4afc7e220b30749fddd9b3e1aa1952b56270033
kusakabe-lvgs <takuto.kusakabe@leverages.jp>
2020-01-29 19:28:08 +0900

    wip

--- lib/cluster/ecs.ts
+++ lib/cluster/ecs.ts
@@ -1,12 +1,44 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
 import * as ecs from '@aws-cdk/aws-ecs';
+import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
+
+interface IClusterEcsStackProps extends cdk.StackProps {
+  context: any;
+  env: any;
+}
 
 export class ClusterEcsStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props: IClusterEcsStackProps) {
     super(scope, id, props);
 
+
+    // VPC
+    const vpc = ec2.Vpc.fromLookup(
+      this,
+      'VPC',
+      {
+        vpcId: props.context.vpcId
+      }
+    );
+
+    // Cluster
     const cluster = new ecs.Cluster(this, 'ECSCluster', {
       vpc: vpc
-    })
+    });
+
+    // Fargate Service
+    // new ecs.LoadBalancedFargateService(this, 'FargateService', {
+
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'FargateService', {
+      cluster: cluster,
+      cpu: 256,
+      desiredCount: 6,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+      },
+      memoryLimitMiB: 2048,
+      publicLoadBalancer: true
+    });
   }
 }
--- lib/network/subnet.ts
+++ lib/network/subnet.ts
@@ -1,101 +1,70 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
-import * as ecs from '@aws-cdk/aws-ecs';
 
-
-interface NetworkSubnetStackProps extends cdk.StackProps {
-  vpc: string;
+interface INetworkSubnetStackProps extends cdk.StackProps {
+  context: any;
+  env: any;
 }
 
 export class NetworkSubnetStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props: NetworkSubnetStackProps) {
+  constructor(scope: cdk.Construct, id: string, props: INetworkSubnetStackProps) {
     super(scope, id, props);
 
-    // VPC
-    // const vpc = new ec2.Vpc(this, 'MyVpc', {
-    //   maxAzs: 2
-    // });
 
+    // // VPC
+    // const vpc = ec2.Vpc.fromLookup(
+    //   this,
+    //   'VPC',
+    //   {
+    //     vpcId: props.context.vpcId
+    //   }
+    // );
+
+    // Subnet
     interface IAz {
       availabilityZone: string;
-      cidrBlock: string[];
+      cidrBlock: string;
       vpcId: string;
+      mapPublicIpOnLaunch: boolean;
     }
 
     const azProps: IAz[] = [
       {
         'availabilityZone': 'ap-northeast-1a',
-        'cidrBlock': [
-          '10.0.10.0/24',
-          '10.0.11.0/24',
-          '10.0.12.0/24',
-          '10.0.13.0/24'
-        ],
-        'vpcId': ''
+        'cidrBlock': '10.0.10.0/24',
+        'vpcId': props.context.vpcId,
+        'mapPublicIpOnLaunch': true
+      },
+      {
+        'availabilityZone': 'ap-northeast-1c',
+        'cidrBlock': '10.0.11.0/24',
+        'vpcId': props.context.vpcId,
+        'mapPublicIpOnLaunch': true
+      },
+      {
+        'availabilityZone': 'ap-northeast-1a',
+        'cidrBlock': '10.0.12.0/24',
+        'vpcId': props.context.vpcId,
+        'mapPublicIpOnLaunch': false
+      },
+      {
+        'availabilityZone': 'ap-northeast-1c',
+        'cidrBlock': '10.0.13.0/24',
+        'vpcId': props.context.vpcId,
+        'mapPublicIpOnLaunch': false
       }
     ];
 
-    const Subnets = azProps.forEach((props, index) => {
+    const _Subnets = azProps.forEach((parameter) => {
       new ec2.Subnet(
         this,
-        `scout-private-${props.availabilityZone.slice(-1)}-subnet-${index}`,
+        `scout-${parameter.mapPublicIpOnLaunch ? 'public' : 'private'}-${parameter.availabilityZone.slice(-1)}-subnet`,
         {
-          availabilityZone: props.availabilityZone,
-          cidrBlock: props.cidrBlock[index],
-          vpcId: props.vpcId
+          availabilityZone: parameter.availabilityZone,
+          cidrBlock: parameter.cidrBlock,
+          vpcId: parameter.vpcId,
+          mapPublicIpOnLaunch: parameter.mapPublicIpOnLaunch
         });
     });
-
-    // const publicSubnets = azProps.forEach(props => {
-    //   new ec2.PrivateSubnet(
-    //     this,
-    //     `scout-public-${props.availabilityZone.slice(-1)}-subnet`,
-    //     props
-    //   );
-    // });
-
-    // const privateSubnets = azProps.forEach(props => {
-    //   new ec2.PrivateSubnet(
-    //     this,
-    //     `scout-private-${props.availabilityZone.slice(-1)}-subnet`,
-    //     props);
-    // });
-
-
-
-    // TODO: 後で分離 ここからcluster
-    // const cluster = new ecs.Cluster(this, 'FargateCluster', {
-    //   vpc: vpc,
-    // });
-
-    // cluster.addCapacity('DefaultAutoScalingGroupCapacity', {
-    //   instanceType: new ec2.InstanceType('t3.small'),
-    //   desiredCapacity: 3,
-    // });
-
-    // const taskDefinition = new ecs.Ec2TaskDefinition(this, 'TaskDef');
-
-    // taskDefinition.addContainer('DefaultContainer', {
-    //   image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
-    //   memoryLimitMiB: 512,
-    // });
-
-    // const ecsService = new ecs.Ec2Service(this, 'Service', {
-    //   cluster,
-    //   taskDefinition,
-    // });
-
-
-
-
-
-    // new ecs.LoadBalancedFargateService(this, 'FargateService', {
-    //   cluster: cluster,
-    //   cpu: '256',
-    //   desiredCount: 1,
-    //   image: ecs.ContainerImage.fromDockerHub('amazon/amazon-ecs-sample'),
-    //   memoryMiB: '512',
-    //   publicLoadBalancer: true
-    // });
   }
 }
